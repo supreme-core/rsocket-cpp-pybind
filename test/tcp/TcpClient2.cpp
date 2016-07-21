eebbf41e5d0cbf60ef8a023445e9eeff5952ea46
Ondrej Lehecka <lehecka@github.com>
2016-07-21 13:27:20 -0700

    test test foo

---
+++ test/tcp/TcpClient2.cpp
@@ -0,0 +1,17 @@
+#include <gmock/gmock.h>
+#include <glog/logging.h>
+
+DEFINE_string(host, "localhost", "host to connect to");
+DEFINE_int32(port, 9898, "host:port to connect to");
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+
+
+  CHECK(FLAGS_host.empty()) << "ahoj vole";
+  LOG(INFO) << "smrdis";
+
+  return 0;
+}
