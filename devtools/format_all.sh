e16cfca69cc087bba405ed77fd315e43fdde8d27
Ondrej Lehecka <lehecka@dev2636.prn1.facebook.com>
2016-08-01 15:18:58 -0700

    fixing TARGETS integration

--- devtools/format_all.sh
+++ devtools/format_all.sh
@@ -2,6 +2,7 @@
 set -xue
 
 cd "$(dirname "$0")/.."
-find src/ -type f '(' -name '*.cpp' -o -name '*.h' ')' -exec clang-format -i {} \;
+find src/ test/ tck-test/ -type f '(' -name '*.cpp' -o -name '*.h' ')' -exec clang-format -i {} \;
 
 # EOF
+
