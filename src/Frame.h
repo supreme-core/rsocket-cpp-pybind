88645f14f5d08bdff0042e1e19d05abed792c1c9
Yuri Schimke <yuri@schimke.ee>
2016-11-14 16:07:14 +0000

    max request(N) is signed int32

--- src/Frame.h
+++ src/Frame.h
@@ -282,14 +282,12 @@ class Frame_REQUEST_N {
   static constexpr bool Trait_CarriesAllowance = true;
 
   /*
-   * Maximum value for ReactiveSocket Subscription::request.
+   * Maximum value for ReactiveSocket Subscription::request. 
+   * Value is a signed int, however negative values are not allowed.
    *
    * n.b. this is less than size_t because of the Frame encoding restrictions.
-   *
-   * Publishers may optionally consider kMaxRequestN as infinite
-   * and avoid future accounting.
    */
-  static constexpr size_t kMaxRequestN = std::numeric_limits<uint32_t>::max();
+  static constexpr size_t kMaxRequestN = std::numeric_limits<int32_t>::max();
 
   Frame_REQUEST_N() = default;
   Frame_REQUEST_N(StreamId streamId, uint32_t requestN)
