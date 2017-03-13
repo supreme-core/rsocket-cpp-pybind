supreme-core/rsocket-cpp-pybind

commit 3b39c48e5928f3767975c04693d4af58ceb0f777

Let keepalive implementations send data
Summary: This also makes the implementation a bit cleaner.

Reviewed By: lehecka

Differential Revision: D4650243

fbshipit-source-id: 7ce01680b221efb6d9d68a21a2a6816ce3c976dc

src/ConnectionAutomaton.cpp
@@ -602,8 +602,8 @@ void ConnectionAutomaton::handleUnknownStream(
 }
 /// @}
 
-void ConnectionAutomaton::sendKeepalive() {
-  sendKeepalive(FrameFlags_KEEPALIVE_RESPOND, folly::IOBuf::create(0));
+void ConnectionAutomaton::sendKeepalive(std::unique_ptr<folly::IOBuf> data) {
+  sendKeepalive(FrameFlags_KEEPALIVE_RESPOND, std::move(data));
 }
 
 void ConnectionAutomaton::sendKeepalive(

src/ConnectionAutomaton.h
@@ -55,7 +55,8 @@ class FrameSink {
   /// StreamAutomatonBase attached to this ConnectionAutomaton.
   virtual void disconnectOrCloseWithError(Frame_ERROR&& error) = 0;
 
-  virtual void sendKeepalive() = 0;
+  virtual void sendKeepalive(
+    std::unique_ptr<folly::IOBuf> data = folly::IOBuf::create(0)) = 0;
 };
 
 /// Handles connection-level frames and (de)multiplexes streams.
@@ -147,7 +148,7 @@ class ConnectionAutomaton final
   void useStreamState(std::shared_ptr<StreamState> streamState);
   /// @}
 
-  void sendKeepalive() override;
+  void sendKeepalive(std::unique_ptr<folly::IOBuf> data) override;
 
   void setResumable(bool resumable);
   Frame_RESUME createResumeFrame(const ResumeIdentificationToken& token) const;

commit 43e0ea9e5fd3a47449191099daec5e6bb908b51b

Ensure KeepaliveTimerTest.cpp is built properly.
Summary: KeepaliveTimerTest.cpp build broke after https://github.com/ReactiveSocket/reactivesocket-cpp/commit/3b39c48e5928f3767975c04693d4af58ceb0f777

Reviewed By: lehecka

Differential Revision: D4694299

fbshipit-source-id: c09b820d613b3cc2a28d76d934d41ccf9b93a6c8

test/folly/FollyKeepaliveTimerTest.cpp
@@ -17,7 +17,13 @@ using namespace ::reactivesocket;
 namespace {
 class MockConnectionAutomaton : public FrameSink {
  public:
-  MOCK_METHOD0(sendKeepalive, void());
+  // MOCK_METHOD doesn't take functions with unique_ptr args.
+  // A workaround for sendKeepalive method.
+  virtual void sendKeepalive(std::unique_ptr<folly::IOBuf> b) override {
+    sendKeepalive_(b);
+  }
+  MOCK_METHOD1(sendKeepalive_, void(std::unique_ptr<folly::IOBuf>&));
+
   MOCK_METHOD1(disconnectOrCloseWithError_, void(Frame_ERROR&));
 
   void disconnectOrCloseWithError(Frame_ERROR&& error) override {
@@ -30,7 +36,7 @@ TEST(FollyKeepaliveTimerTest, StartStopWithResponse) {
   auto connectionAutomaton =
       std::make_shared<NiceMock<MockConnectionAutomaton>>();
 
-  EXPECT_CALL(*connectionAutomaton, sendKeepalive()).Times(2);
+  EXPECT_CALL(*connectionAutomaton, sendKeepalive_(_)).Times(2);
 
   folly::EventBase eventBase;
 
@@ -51,7 +57,7 @@ TEST(FollyKeepaliveTimerTest, NoResponse) {
   auto connectionAutomaton =
       std::make_shared<StrictMock<MockConnectionAutomaton>>();
 
-  EXPECT_CALL(*connectionAutomaton, sendKeepalive()).Times(1);
+  EXPECT_CALL(*connectionAutomaton, sendKeepalive_(_)).Times(1);
   EXPECT_CALL(*connectionAutomaton, disconnectOrCloseWithError_(_)).Times(1);
 
   folly::EventBase eventBase;