9644669fd0a7b51c775751c8fe5264d33e7c0f7f
Ben Christensen <benjchristensen@gmail.com>
2017-05-23 10:19:24 -0700

    RequestChannelTest (#483)

---
+++ test/RequestChannelTest.cpp
@@ -0,0 +1,71 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <chrono>
+#include <thread>
+
+#include "RSocketTests.h"
+#include "yarpl/Flowable.h"
+#include "yarpl/flowable/TestSubscriber.h"
+
+using namespace yarpl;
+using namespace yarpl::flowable;
+using namespace rsocket;
+using namespace rsocket::tests;
+using namespace rsocket::tests::client_server;
+using namespace std::chrono_literals;
+
+/**
+ * Test a finite stream both directions.
+ */
+class TestHandlerHello : public rsocket::RSocketResponder {
+ public:
+  /// Handles a new inbound Stream requested by the other end.
+  yarpl::Reference<Flowable<rsocket::Payload>> handleRequestChannel(
+      rsocket::Payload initialPayload,
+      yarpl::Reference<Flowable<rsocket::Payload>> request,
+      rsocket::StreamId streamId) override {
+    // say "Hello" to each name on the input stream
+    return request->map([initialPayload = std::move(initialPayload)](
+        Payload p) {
+      std::stringstream ss;
+      ss << "[" << initialPayload.cloneDataToString() << "] "
+         << "Hello " << p.moveDataToString() << "!";
+      std::string s = ss.str();
+
+      return Payload(s);
+    });
+  }
+};
+
+TEST(RequestChannelTest, Hello) {
+  auto port = randPort();
+  auto server = makeServer(port, std::make_shared<TestHandlerHello>());
+  auto client = makeClient(port);
+  auto requester = client->connect().get();
+
+  auto ts = TestSubscriber<std::string>::create();
+  requester
+      ->requestChannel(
+          Flowables::justN({"/hello", "Bob", "Jane"})->map([](std::string v) {
+            return Payload(v);
+          }))
+      ->map([](auto p) { return p.moveDataToString(); })
+      ->subscribe(ts);
+
+  ts->awaitTerminalEvent();
+  ts->assertSuccess();
+  ts->assertValueCount(2);
+  // assert that we echo back the 2nd and 3rd request values
+  // with the 1st initial payload prepended to each
+  ts->assertValueAt(0, "[/hello] Hello Bob!");
+  ts->assertValueAt(1, "[/hello] Hello Jane!");
+}
+
+// TODO complete from requester, responder continues
+// TODO complete from responder, requester continues
+// TODO cancel from requester, shuts down
+// TODO flow control from requester to responder
+// TODO flow control from responder to requester
+// TODO failure on responder, requester sees
+// TODO failure on request, requester sees
+// TODO failure from requester ... what happens?
