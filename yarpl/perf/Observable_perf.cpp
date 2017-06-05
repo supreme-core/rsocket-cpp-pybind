edb28dee6a04ab024ba4effe5cc7ce9ae70b5be9
somasun <manikandan@fb.com>
2017-06-05 13:51:32 -0700

    enable yarpl benchmarks (#493)

--- yarpl/perf/Observable_perf.cpp
+++ yarpl/perf/Observable_perf.cpp
@@ -3,13 +3,14 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 #include "yarpl/Observable.h"
+#include "yarpl/observable/Observers.h"
 
 using namespace yarpl::observable;
 
 static void Observable_OnNextOne_ConstructOnly(benchmark::State& state) {
   while (state.KeepRunning()) {
-    auto a = Observable<int>::create([](std::unique_ptr<Observer<int>> obs) {
-      obs->onSubscribe(Subscription::create());
+    auto a = Observable<int>::create([](yarpl::Reference<Observer<int>> obs) {
+      obs->onSubscribe(Subscriptions::empty());
       obs->onNext(1);
       obs->onComplete();
     });
@@ -18,30 +19,32 @@ static void Observable_OnNextOne_ConstructOnly(benchmark::State& state) {
 BENCHMARK(Observable_OnNextOne_ConstructOnly);
 
 static void Observable_OnNextOne_SubscribeOnly(benchmark::State& state) {
-  auto a = Observable<int>::create([](std::unique_ptr<Observer<int>> obs) {
-    obs->onSubscribe(Subscription::create());
+  auto a = Observable<int>::create([](yarpl::Reference<Observer<int>> obs) {
+    obs->onSubscribe(Subscriptions::empty());
     obs->onNext(1);
     obs->onComplete();
   });
   while (state.KeepRunning()) {
-    a->subscribe(Observer<int>::create([](int value) { /* do nothing */ }));
+    a->subscribe(Observers::create<int>([](int value) { /* do nothing */ }));
   }
 }
 BENCHMARK(Observable_OnNextOne_SubscribeOnly);
 
 static void Observable_OnNextN(benchmark::State& state) {
   auto a =
-      Observable<int>::create([&state](std::unique_ptr<Observer<int>> obs) {
-        obs->onSubscribe(Subscription::create());
+      Observable<int>::create([&state](yarpl::Reference<Observer<int>> obs) {
+        obs->onSubscribe(Subscriptions::empty());
         for (int i = 0; i < state.range(0); i++) {
           obs->onNext(i);
         }
         obs->onComplete();
       });
   while (state.KeepRunning()) {
-    a->subscribe(Observer<int>::create([](int value) { /* do nothing */ }));
+    a->subscribe(Observers::create<int>([](int value) {}));
   }
 }
 
 // Register the function as a benchmark
 BENCHMARK(Observable_OnNextN)->Arg(100)->Arg(10000)->Arg(1000000);
+
+BENCHMARK_MAIN()
--- yarpl/perf/yarpl-perf.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-#include <benchmark/benchmark.h>
-
-// main method for all perf tests in other files
-BENCHMARK_MAIN();
