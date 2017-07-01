b1884f5ade9576678b8d9c7450e3894b83896a2b
Alexander Malyshev <lex.malyshev@gmail.com>
2017-06-30 17:10:39 -0700

    Fix RSocketClientServer.ConnectManyAsync test (#537)
    It would create a vector of RSocketRequester futures, but destroy the clients
    they were bound to.  As such the clients would get destructed before they could
    fulfill any of the requesters.

--- test/RSocketClientServerTest.cpp
+++ test/RSocketClientServerTest.cpp
@@ -26,16 +26,17 @@ TEST(RSocketClientServer, ConnectManySync) {
   }
 }
 
-// TODO: Investigate why this hangs (even with i < 2).
-TEST(RSocketClientServer, DISABLED_ConnectManyAsync) {
+TEST(RSocketClientServer, ConnectManyAsync) {
   auto server = makeServer(std::make_shared<HelloStreamRequestHandler>());
 
+  std::vector<std::unique_ptr<RSocketClient>> clients;
   std::vector<folly::Future<folly::Unit>> futures;
 
   for (size_t i = 0; i < 100; ++i) {
     auto client = makeClient(*server->listeningPort());
     auto requester = client->connect();
 
+    clients.push_back(std::move(client));
     futures.push_back(requester.unit());
   }
 
