13b4ba9e2277014349a0f53d53b916d06337f301
somasun <manikandan@fb.com>
2017-08-08 22:21:32 -0700

    refactor resumption examples (#628)

---
+++ examples/resumption/ColdResumption_Client.cpp
@@ -0,0 +1,13 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+
+#include <folly/init/Init.h>
+
+int main(int argc, char* argv[]) {
+
+  folly::init(&argc, &argv);
+
+  // code to be added
+
+  return 0;
+}
